704813fe8641e3abf1ea0074c95e92bf4307a3d5
aditya90205 <adityasrivastav90205@gmail.com>
2024-09-25 13:20:01 +0530

    feat: Add messages.json file with sample messages
    chore: Update package.json dependencies
    
    feat: Add RootLayout component
    
    chore: Update layout.tsx file
    
    feat: Add Separator component
    
    feat: Add Navbar component
    
    feat: Add Switch component
    
    feat: Add Card components
    
    feat: Add MessageCard component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
-import "@/app/globals.css";
-import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "@/app/globals.css";
+import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster";
+import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,10 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        {children}
-      </body>
+      <AuthProvider>
+        <body className={inter.className}>
+          <Navbar />
+          {children}
+          <Toaster />
+        </body>
+      </AuthProvider>
     </html>
   );
 }
